a797496850039f1932fa32f85e24422a30e8ff18
JoseLRiano <joselriano@yahoo.com>
2020-08-20 01:38:35 -0400

    MongoDB and Mongoose added

--- app.js
+++ app.js
@@ -1,27 +1,43 @@
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 // express app
 const app = express();
 
+// Connect to mogodb
+const dbURI = 'mongodb+srv://nodeTutorial:@cluster0.sbq0s.mongodb.net/node-tutorial?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) =>  app.listen(3000))
+    .catch((err) => console.log(err));
+
 // Register view engine
 app.set('view engine', 'ejs');
 
-//Listen for requests
-app.listen(3000);
+
+// Middleware & static files
+app.use(express.static('public'));
+
+app.use(morgan('dev'));
+
 
 app.get('/', (req, res) => {
-    //res.send('<p>Home Page</p>');
-    const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet.' },
-        {title: 'Mario finds starts', snippet: 'Lorem ipsum dolor sit amet.' },
-        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet.' }
-    ]
-    res.render('index', { title: 'Home' , blogs });
+    res.redirect('/blogs');
 }); 
 app.get('/about', (req, res) => {
     //res.send('<p>About Page</p>');
     res.render('about', { title: 'About' })
 }); 
+
+// Blog routes
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({ createdAt: -1 })
+        .then((result) => {
+            res.render('index', { title: 'All Blogs', blogs: result})
+        })
+        .catch((err) => console.log(err));
+})
 
 app.get('/blogs/create', (req, res) => {
     res.render('create', { title: 'Create a new Blog' });
