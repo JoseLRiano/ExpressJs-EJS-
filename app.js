43889c3d2d9df81e572aefd5bd74819fbedb0abd
JoseLRiano <joselriano@yahoo.com>
2020-08-20 19:07:31 -0400

    Delete create and display one blog also MVC was implemente

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const Blog = require('./models/blog');
+const blogRoutes = require('./routes/blogRoutes');
+
 
 // express app
 const app = express();
 
 // Connect to mogodb
-const dbURI = 'mongodb+srv://nodeTutorial:@cluster0.sbq0s.mongodb.net/node-tutorial?retryWrites=true&w=majority';
+const dbURI = '';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then((result) =>  app.listen(3000))
     .catch((err) => console.log(err));
@@ -18,7 +19,7 @@ app.set('view engine', 'ejs');
 
 // Middleware & static files
 app.use(express.static('public'));
-
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 
@@ -30,18 +31,9 @@ app.get('/about', (req, res) => {
     res.render('about', { title: 'About' })
 }); 
 
-// Blog routes
-app.get('/blogs', (req, res) => {
-    Blog.find().sort({ createdAt: -1 })
-        .then((result) => {
-            res.render('index', { title: 'All Blogs', blogs: result})
-        })
-        .catch((err) => console.log(err));
-})
+// Blog Routes
+app.use("/blogs", blogRoutes);
 
-app.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'Create a new Blog' });
-})
 
 // 404 page
 app.use((req,res) => {
